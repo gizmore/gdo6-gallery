41d46128f39977fa606ecf65da6dd367a4521f52
gizmore <gizmore@wechall.net>
2019-01-10 00:43:46 +0100

    6.09-r11_fixes

--- lang/gallery_en.php
+++ lang/gallery_en.php
@@ -6,4 +6,5 @@ return array(
 'link_gallery_add' => 'Create a new Gallery',
 'list_gallerylist' => '[%s] Galleries',
 'gallery_li2' => '%s images by %s on %s',
+'err_gallery_view_permission' => 'You are not allowed to view this gallery: %s',
 );
