f24f368223cd058881ff52a46f5c83f9afe5ba33
gizmore <gizmore@wechall.net>
2019-05-30 16:51:32 +0200

    6.09-r451_sync

--- lang/gallery_en.php
+++ lang/gallery_en.php
@@ -1,10 +1,15 @@
 <?php
 return array(
 'gdo_gallery' => 'Gallery',
+
 'link_gallery' => 'Galleries',
 'link_your_gallery' => 'Your gallery',
 'link_gallery_add' => 'Create a new Gallery',
 'list_gallerylist' => '[%s] Galleries',
+
 'gallery_li2' => '%s images by %s on %s',
+
 'err_gallery_view_permission' => 'You are not allowed to view this gallery: %s',
+
+'cfg_gallery_acl' => 'Gallery visibility',
 );
