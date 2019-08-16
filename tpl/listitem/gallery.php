3ade07d2599d9366415d9c1c0acf1b8df0b46bd8
gizmore <gizmore@wechall.net>
2019-08-16 16:31:43 +0200

    6.10-r14_lang

--- tpl/listitem/gallery.php
+++ tpl/listitem/gallery.php
@@ -1,21 +1,27 @@
 <?php
-use GDO\Avatar\GDO_Avatar;
 use GDO\Gallery\GDO_Gallery;
 use GDO\UI\GDT_EditButton;
-use GDO\UI\GDT_Icon;
+use GDO\UI\GDT_ListItem;
+use GDO\UI\GDT_Button;
 use GDO\User\GDO_User;
+use GDO\UI\GDT_Title;
+use GDO\UI\GDT_Paragraph;
+use GDO\Avatar\GDT_Avatar;
+/**
+ * @var $gallery GDO_Gallery
+ */
 $gallery instanceof GDO_Gallery;
-?>
-<md-list-item class="md-2-line" href="<?= $gallery->href_show(); ?>">
-	<?= GDO_Avatar::renderAvatar($gallery->getCreator()); ?>
-  <div class="md-list-item-text" layout="column">
-	<h2><?= html($gallery->getTitle()); ?></h2>
-	<h3><?= t('gallery_li2', [$gallery->getImageCount(), $gallery->getCreator()->displayName(), $gallery->displayDate()]); ?></h3>
-	<p><?= $gallery->displayDescription(); ?></p>
-  </div>
-  <?php if ($gallery->canEdit(GDO_User::current())) : ?>
-	<?= GDT_EditButton::make()->href(href('Gallery', 'Crud', "&id={$gallery->getID()}"))->render(); ?>
-  <?php endif; ?>
-  
-  <?= GDT_Icon::iconS('arrow_right'); ?>
-</md-list-item>
+
+$li = GDT_ListItem::make();
+$li->title(GDT_Title::make()->initial($gallery->getTitle()));
+$subtitle = t('gallery_li2', [$gallery->getImageCount(), $gallery->getCreator()->displayName(), $gallery->displayDate()]);
+$li->subtitle(GDT_Title::make()->initial($subtitle));
+$li->subtext(GDT_Paragraph::make()->initial($gallery->displayDescription()));
+$li->image(GDT_Avatar::make()->user($gallery->getCreator()));
+$actions = $li->actions();
+if ($gallery->canEdit(GDO_User::current()))
+{
+	$actions->addField(GDT_EditButton::make()->href(href('Gallery', 'Crud', "&id={$gallery->getID()}")));
+}
+$actions->addField(GDT_Button::make()->href($gallery->href_show())->icon('view')->label('btn_view'));
+echo $li->render();
